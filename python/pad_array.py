b10c4f6e2269512ff8e2e5cb8ecab60279a6f5e5
robertuptc <robertuptc@gmail.com>
2025-03-11 12:25:47 -0400

    python problem finished

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,3 +1,8 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
-    pass 
+    if len(array) > min_size or min_size < 0:
+        return array
+    else:
+        while len(array) < min_size:
+            array.append(value)
+    return array
